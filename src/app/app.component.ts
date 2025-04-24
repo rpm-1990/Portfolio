7f285d65833f48f2af06f7e61b0f5d7f31b87716
rpm-1990 <reddypm1990@gmail.com>
2025-04-23 20:49:46 -0400

    implementation of hash function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { OnInit } from '@angular/core';
-import { Component } from '@angular/core';
-import { APP_DEPS } from './app.config';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './layouts/landing/landing.component';
+import { AboutComponent } from './layouts/about/about.component';
+import { ServicesComponent } from './layouts/services/services.component';
+import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
+import { PortfolioComponent } from './layouts/portfolio/portfolio.component';
+import { ResumeComponent } from './layouts/resume/resume.component';
+import { ContactsComponent } from './layouts/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [APP_DEPS],
+  imports: [
+    HeaderComponent,
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent,
+    ScrollTopComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ContactsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -18,5 +35,4 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.themeService.initTheme();
   }
-
 }
